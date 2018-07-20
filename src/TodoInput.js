6a3de80650ce8a8930ecfc9892d1cd0da5dc5f18
unknown <men.nasies@gmail.com>
2018-07-20 22:28:45 +0300

    Create the TodoInput component

---
+++ src/TodoInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoInput = ({handleChange,handleClickAdd}) => {
+  return (
+    <div>
+      <input 
+          onChange = {handleChange}
+          placeholder = "Add new todo" 
+          type="text"
+      />
+      <button onClick={handleClickAdd}> Adds </button>
+    </div>
+  )
+}
